1dcf452c9f67b39ea01b3447b7ca9dfe8bdcee91
annikaeld <130365506+annikaeld@users.noreply.github.com>
2025-05-17 22:41:00 +0200

    style feed #11

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./**/*.{html,js}", "!./node_modules/**/*"],
   theme: {
     extend: {},
+    screens: {
+      xs: "450px",
+      sm: "640px",
+      md: "768px",
+    },
   },
   plugins: [],
 };
